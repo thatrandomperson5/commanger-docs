f1929717d067f53e1040f4b4ad5ab56af68b8324
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-07-06 17:56:36 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
-
+html_favicon = 'favicon.ico'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
